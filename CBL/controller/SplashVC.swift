200732e4d9a34512a889165c41e3993ff32b3739
Biswajit <bswjtbanik@gmail.com>
2018-08-01 11:53:12 +0600

    animation add

--- CBL/controller/SplashVC.swift
+++ CBL/controller/SplashVC.swift
@@ -20,10 +20,19 @@ class SplashVC: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
             self.performSegue(withIdentifier: "nextPage", sender: nil)
             
-            
-         
         }
     
+    }
+    override func viewWillAppear(_ animated: Bool){
+           ebl_skybankinglogo.transform = CGAffineTransform(translationX: 0, y: +ebl_skybankinglogo.frame.height)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2, delay: 0, options: [.curveEaseOut],
+                       animations: {
+                        self.ebl_skybankinglogo.center.y -= self.view.bounds.height - 250
+                        self.view.layoutIfNeeded()
+        }, completion: nil)
     }
  
     
